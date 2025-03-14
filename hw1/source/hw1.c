d6abe8deac41b480bcb9ef3b1900e0deb2e7f724
Zachary Heras <zacharyheras@gmail.com>
2025-03-14 19:58:09 -0400

    added comments to hw1.c

--- hw1/source/hw1.c
+++ hw1/source/hw1.c
@@ -8,14 +8,6 @@ Homework #: 1
 #include <stdlib.h>
 #include <sys/time.h>
 
-double get_time(void) {
-    struct timeval tval;
-
-    gettimeofday(&tval, NULL);
-
-    return( (double)tval.tv_sec + (double)tval.tv_usec/1000000.0 );
-}
-
 int **alloc_board(int P, int Q) {
     int i;
     int *p, **a;
@@ -28,63 +20,103 @@ int **alloc_board(int P, int Q) {
 
     /* for row major storage */
     for (i = 0; i < P; i++)
-        a[i] = &p[i*Q];
+        game_board[i] = &p[i*Q];
 
     return a;
 }
 
-void **init_board(int **a, int mrows, int ncols) {
-    int i,j;
+/*
+ * This function randomly initializes each non-ghost
+ * cell to a value of 0 for dead or 1 for alive
+ */
+void **init_board(int **game_board, int mrows, int ncols) {
+    int i;
+    int j;
 
-    for (i=1; i<mrows-1; i++) {
-        for (j=1; j<ncols-1; j++) {
-            a[i][j] = (drand48() < 0.5) ? 0 : 1;
+    for (i=1; i < mrows-1; i++) {
+        for (j=1; j < ncols-1; j++) {
+            game_board[i][j] = (drand48() < 0.5) ? 0 : 1;
         }
     }
 }
 
-void **update_ghost_cells(int **a, int mrows, int ncols) {
-    int i,j;
+/*
+ * This function updates the ghost cells based on the
+ * current non-ghost cells
+ */
+void **update_ghost_cells(int **game_board, int mrows, int ncols) {
+    int i;
+    int j;
 
-    a[0][0] = a[mrows-2][ncols-2];
-    a[0][ncols-1] = a[mrows-2][1];
-    a[mrows-1][0] = a[1][ncols-2];
-    a[mrows-1][ncols-1] = a[1][1];
+    // These lines update the corner ghost cells
+    game_board[0][0] = game_board[mrows-2][ncols-2];
+    game_board[0][ncols-1] = game_board[mrows-2][1];
+    game_board[mrows-1][0] = game_board[1][ncols-2];
+    game_board[mrows-1][ncols-1] = game_board[1][1];
 
-    for (i=1; i<mrows-1; i++)
-        a[i][0] = a[i][ncols-2];
+    // This updates the ghost cells in the leftmost column
+    for (i=1; i < mrows-1; i++)
+        game_board[i][0] = game_board[i][ncols-2];
 
-    for (i=1; i<mrows-1; i++)
-        a[i][ncols-1] = a[i][1];
+    // This updates the ghost cells in the rightmost column
+    for (i=1; i < mrows-1; i++)
+        game_board[i][ncols-1] = game_board[i][1];
 
-    for (j=1; j<ncols-1; j++)
-        a[0][j] = a[mrows-2][j];
+    // This updates the ghost cells in the topmost row
+    for (j=1; j < ncols-1; j++)
+        game_board[0][j] = game_board[mrows-2][j];
 
-    for (j=1; j<ncols-1; j++)
-        a[mrows-1][j] = a[1][j];
+    // This updates the ghost cells in the bottommost row
+    for (j=1; j < ncols-1; j++)
+        game_board[mrows-1][j] = game_board[1][j];
 }
 
-int is_alive(int **a, int i, int j) {
+/*
+ * This function prints the game board
+ */
+void print_board(int **game_board, int mrows, int ncols) {
+    int i;
+    int j;
+  
+    for (i=0; i < mrows; i++) {
+        for (j=0; j < ncols; j++)
+              printf("%d ", game_board[i][j]);
+        printf("\n");
+    }
+
+    printf("\n");
+}
+
+double get_time(void) {
+    struct timeval tval;
+
+    gettimeofday(&tval, NULL);
+
+    return( (double)tval.tv_sec + (double)tval.tv_usec/1000000.0 );
+}
+
+/*
+ * This function determines if a cell will
+ * be alive in the next iteration
+ */
+int is_alive(int **game_board, int i, int j) {
     int num_neighbors = 0;
 
-    num_neighbors += a[i-1][j-1] == 1 ? 1 : 0;
-    num_neighbors += a[i-1][j]   == 1 ? 1 : 0;
-    num_neighbors += a[i-1][j+1] == 1 ? 1 : 0;
+    num_neighbors += game_board[i-1][j-1] == 1 ? 1 : 0;
+    num_neighbors += game_board[i-1][j]   == 1 ? 1 : 0;
+    num_neighbors += game_board[i-1][j+1] == 1 ? 1 : 0;
 
-    num_neighbors += a[i][j-1] == 1 ? 1 : 0;
-    num_neighbors += a[i][j+1] == 1 ? 1 : 0;
+    num_neighbors += game_board[i][j-1] == 1 ? 1 : 0;
+    num_neighbors += game_board[i][j+1] == 1 ? 1 : 0;
 
-    num_neighbors += a[i+1][j-1] == 1 ? 1 : 0;
-    num_neighbors += a[i+1][j]   == 1 ? 1 : 0;
-    num_neighbors += a[i+1][j+1] == 1 ? 1 : 0;
-
-    // printf("i:%d j:%d neighbors: %d", i, j, num_neighbors);
-    // printf("\n");
+    num_neighbors += game_board[i+1][j-1] == 1 ? 1 : 0;
+    num_neighbors += game_board[i+1][j]   == 1 ? 1 : 0;
+    num_neighbors += game_board[i+1][j+1] == 1 ? 1 : 0;
 
     if (num_neighbors == 3)
         return 1;
 
-    if (!a[i][j])
+    if (!game_board[i][j])
         return 0;
 
     if (num_neighbors == 2)
@@ -94,93 +126,76 @@ int is_alive(int **a, int i, int j) {
 
 }
 
-int update_board(int **a, int **b, int mrows, int ncols) {
-    int i, j;
-    int changed = 0;
-    int temp = 0;
+/*
+ * This function updates each cell's status and returns true
+ * if the status of any cell changed
+ */
+int update_board(int **game_board, int temp_board, int mrows, int ncols) {
+    int i;
+    int j;
+    int updated = 0;
 
-    for (i=1; i<mrows-1; i++) { 
-        for (j=1; j<ncols-1; j++) {
-            b[i][j] = is_alive(a, i, j);
+    for (i=1; i < mrows-1; i++) { 
+        for (j=1; j < ncols-1; j++) {
+            temp_board[i][j] = is_alive(a, i, j);
             
-            if (a[i][j] != b[i][j])
+            if (game_board[i][j] != temp_board[i][j]) {
                 changed = 1;
+            }
         }
     }
 
-    for (; i<mrows-1; i++) { 
-        for (; j<ncols-1; j++) {
-            temp = a[i][j];
-            b[i][j] = is_alive(a, i, j);
-        }
-    }
-
-   for (i=1; i<mrows; i++) {
-        for (j=1; j<ncols; j++) {
-            a[i][j] = b[i][j];
-        }
-    }
-
-    return changed;
+    return updated;
 }
 
-void print_board(int **a, int mrows, int ncols) {
-    int i,j;
-  
-    for (i=0; i<mrows; i++) {
-        for (j=0; j<ncols; j++)
-              printf("%d ", a[i][j]);
-        printf("\n");
-    }
-    printf("\n");
-}
-
-int main(int argc, char **argv) 
-{
-    int N, max_iterations;
-    int **a=NULL, **b=NULL;
-    double starttime, endtime;
-
+int main(int argc, char **argv) {
     if (argc != 3) {
         printf("Usage: %s <N> <I>\n", argv[0]);
         exit(-1);
     }
     
-    N = atoi(argv[1])+2;
-    max_iterations = atoi(argv[2]);
+    int N = atoi(argv[1])+2;
+    int max_iterations = atoi(argv[2]);
     
-    /* Allocate memory for the current board */
-    a = alloc_board(N, N);
-    b = alloc_board(N, N);
+    // Allocate memory for the game board
+    int **game_board = alloc_board(N, N);
 
-    /* Initialize the game matrix */
+    // Allocate memory for the temp board
+    int **temp_board = alloc_board(N, N);
+
+    // seed the random number generator
     srand48(123456);
+
+    // initialize the game board
     init_board(a, N, N);
     update_ghost_cells(a, N, N);
-    // print_board(a, N, N);
 
-    /* Run game loop until termination or max generation count is reached */
-    starttime = get_time();
+#ifdef DEBUG_PRINT
+    printf("%d\n", iterations);
+    print_board(a, N, N);
+    printf("\n");
+#endif
 
     int terminate = 0;
     int iterations = 0;
+    double starttime = get_time();
 
+    // Run game loop until termination or max generation count is reached
     while (!terminate && iterations < max_iterations) {
         terminate = !update_board(a, b, N, N);
         update_ghost_cells(a, N, N);
-        // print_board(a, N, N);
+        
+#ifdef DEBUG_PRINT
+        printf("%d\n", iterations);
+        print_board(a, N, N);
+        printf("\n");
+#endif
+        
         iterations ++;
-        // printf("%d\n", iterations);
     }
 
-    endtime = get_time();
+    double endtime = get_time();
 
-    /*
-    #ifdef DEBUG_PRINT
-        printboard(a, N, N);
-        printf("\n");
-    #endif
-    */
 
     printf("Time taken = %lf seconds\n", endtime-starttime);
 
